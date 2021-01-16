87aaf175ae4a5c17a69617f7b389eec505a71bc4
Maximous Black <maximousblk@gmail.com>
2021-01-16 16:14:54 +0000

    fix: Unexpected token `?`

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.VERCEL_URL ?? "https://maximousblk.now.sh",
+  siteUrl: process.env.VERCEL_URL || "https://maximousblk.now.sh",
   generateRobotsTxt: true,
   autoLastmod: true
 };
