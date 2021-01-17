5a65b22231f304f5d30c2b90fd25d964c0b444ed
Maximous Black <maximousblk@gmail.com>
2021-01-17 18:04:24 +0000

    larger search icon

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -52,7 +52,7 @@ export default function Blog({ posts }) {
             placeholder={`Search through ${filteredBlogPosts.length} articles`}
             className="px-4 py-2 border border-gray-300 dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
           />
-          <SearchIcon className="absolute right-3 top-3 h-4 w-4 text-gray-400 dark:text-gray-300" />
+          <SearchIcon className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300" />
         </div>
         {!filteredBlogPosts.length && (
           <p className="my-8 self-center text-gray-600 dark:text-gray-400 mb-4">
