c166d67571ac4f457d7e459612cf63ab9f152fc9
Maximous Black <maximousblk@gmail.com>
2021-03-18 02:30:29 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -51,7 +51,7 @@ class MyDocument extends Document {
           <meta content="M4wcsX_DZ9CkpAzZ5rNmUbk1JWl3aLqgxIfB4YG-ozI" name="google-site-verification" />
           <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
         </Head>
-        <body className="bg-white dark:bg-coolgray-900 text-white dark:text-black">
+        <body className="bg-white dark:bg-coolGray-900 text-white dark:text-black">
           <Main />
           <NextScript />
         </body>
