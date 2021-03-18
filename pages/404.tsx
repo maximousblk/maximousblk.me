9f14cf2d972bab76c2e13c839776a868efd2d099
Maximous Black <maximousblk@gmail.com>
2021-03-18 10:58:45 +0530

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -20,12 +20,12 @@ export default function NotFound() {
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           404 – Not Found
         </h1>
-        <p className="text-gray-600 dark:text-gray-400 mb-8">
+        <p className="text-gray-600 dark:text-coolGray-400 mb-8">
           It seems you've found something that used to exist, or you spelled something wrong. Can you double check that
           URL?
         </p>
         <Link href="/">
-          <a className="p-1 sm:p-4 w-64 font-bold mx-auto text-center rounded-md bg-gray-100 dark:bg-gray-900 text-black dark:text-white hover:bg-gray-200 dark:hover:bg-gray-800">
+          <a className="p-1 sm:p-4 w-64 font-bold mx-auto text-center rounded-md bg-gray-100 dark:bg-coolGray-900 text-black dark:text-white hover:bg-gray-200 dark:hover:bg-coolGray-800">
             Return Home
           </a>
         </Link>
