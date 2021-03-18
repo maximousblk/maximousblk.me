76cad835437cce33d32a1019fee9ee3d5396f272
Maximous Black <maximousblk@gmail.com>
2021-03-18 08:00:51 +0530

    Update index.tsx

--- pages/snippets/index.tsx
+++ pages/snippets/index.tsx
@@ -42,12 +42,12 @@ export default function Snippets({ snippets }) {
             type="text"
             onChange={(e) => setSearchValue(e.target.value)}
             placeholder={`Search through ${filteredSnippets.length} snippets`}
-            className="px-4 py-2 border border-gray-300 dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+            className="px-4 py-2 border border-gray-300 dark:border-coolGray-700 focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md bg-white dark:bg-coolGray-800 text-gray-900 dark:text-coolGray-100"
           />
-          <SearchIcon className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300" />
+          <SearchIcon className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-coolGray-300" />
         </div>
         {!filteredSnippets.length && (
-          <p className="my-8 self-center text-gray-600 dark:text-gray-400 mb-4">No snippets found ;-;</p>
+          <p className="my-8 self-center text-gray-600 dark:text-coolGray-400 mb-4">No snippets found ;-;</p>
         )}
         <div className="grid gap-4 grid-cols-1 my-2 w-full mt-4">
           {filteredSnippets.map((snippet) => (
