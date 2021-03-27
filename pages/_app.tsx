9ff9dd82630a97079e544f562337c76da7f06021
Maximous Black <maximousblk@gmail.com>
2021-03-27 13:11:16 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/global.css";
 
 import { MDXProvider } from "@mdx-js/react";
-import { ThemeProvider, useTheme } from "next-themes";
+import { ThemeProvider } from "next-themes";
 import { DefaultSeo } from "next-seo";
 import Head from "next/head";
 
@@ -9,14 +9,11 @@ import SEO from "../next-seo.config";
 import MDXComponents from "@/components/MDXComponents";
 
 export default function App({ Component, pageProps }) {
-  const { theme, setTheme } = useTheme()
-
   return (
     <ThemeProvider attribute="class">
       <MDXProvider components={MDXComponents}>
         <Head>
           <meta content="width=device-width, initial-scale=1" name="viewport" />
-          <meta name="theme-color" content={theme === "dark" ? "#111827" : "#FFFFFF"} />
         </Head>
         <DefaultSeo {...SEO} />
         <Component {...pageProps} />
