840d5f723739dc0d2a6d928b334b4293da2e88d1
Maximous Black <maximousblk@gmail.com>
2021-03-29 13:14:34 +0530

    refactor: remove `MDXProvider` from `_app.js` (#19)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,19 @@
 import "@/styles/global.css";
 
-import { MDXProvider } from "@mdx-js/react";
 import { ThemeProvider } from "next-themes";
 import { DefaultSeo } from "next-seo";
 import Head from "next/head";
 
 import SEO from "../next-seo.config";
-import MDXComponents from "@/components/MDXComponents";
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
-      <MDXProvider components={MDXComponents}>
-        <Head>
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
-        </Head>
-        <DefaultSeo {...SEO} />
-        <Component {...pageProps} />
-      </MDXProvider>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
