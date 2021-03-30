ea6683118d864574033bac38126480d36961ebca
Maximous Black <maximousblk@gmail.com>
2021-03-30 13:45:14 +0530

    chore: update `next` and use `@tailwindcss/jit` (#20)

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -15,7 +15,9 @@ const BlogPost = ({ title, description, slug, publishedAt }: Attributes) => {
         <a className="w-full">
           <div className="mb-4 mt-8 w-full">
             <div className="flex flex-col md:flex-row justify-between">
-              <h4 className="text-lg md:text-xl font-medium mb-2 w-full text-gray-900 dark:text-coolGray-100">{title}</h4>
+              <h4 className="text-lg md:text-xl font-medium mb-2 w-full text-gray-900 dark:text-coolGray-100">
+                {title}
+              </h4>
               <p className="text-gray-500 dark:text-coolGray-500 text-left md:text-right w-32 mb-4 md:mb-0">
                 {publishedAt && format(parseISO(publishedAt), "MMM, yyy")}
               </p>
