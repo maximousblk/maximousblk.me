e1a6bc7da8a28e1b275916cf684aad574781f8a3
Maximous Black <maximousblk@gmail.com>
2021-04-25 11:16:52 +0000

    chore: add style-lint

---
+++ stylelint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: ["tailwind", "apply", "variants", "responsive", "screen"]
+      }
+    ],
+    "property-no-unknown": [
+      true,
+      {
+        ignoreProperties: ["font-named-instance"]
+      }
+    ],
+    "declaration-colon-newline-after": null,
+    "value-list-comma-newline-after": null,
+    "no-invalid-position-at-import-rule": null,
+    "declaration-block-trailing-semicolon": null,
+    "no-descending-specificity": null
+  }
+};
