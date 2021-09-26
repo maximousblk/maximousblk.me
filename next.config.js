c59f276232c64f301700dd6b64f81676c60c3a7d
Maximous Black <maximousblk@gmail.com>
2021-09-26 18:16:43 +0000

    refactor: switch to `next-plugin-preact`

--- next.config.js
+++ next.config.js
@@ -1,61 +1,52 @@
 const withPWA = require("next-pwa");
+const withPreact = require("next-plugin-preact");
 
-module.exports = withPWA({
-  experimental: {
-    nftTracing: true // Fix for `ENOENT: no such file or directory, scandir '/var/task/<path>'`
-  },
-  reactStrictMode: true,
-  images: {
-    domains: ["cdn.maximousblk.me", "octodex.github.com"]
-  },
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat"
-      });
+module.exports = withPWA(
+  withPreact({
+    experimental: {
+      nftTracing: true // Fix for `ENOENT: no such file or directory, scandir '/var/task/<path>'`
+    },
+    reactStrictMode: true,
+    images: {
+      domains: ["cdn.maximousblk.me", "octodex.github.com"]
+    },
+    pwa: {
+      disable: process.env.NODE_ENV === "development",
+      dest: "public"
+    },
+    async rewrites() {
+      return [
+        {
+          source: "/feed/:format*",
+          destination: "/api/feed?f=:format*"
+        },
+        {
+          source: "/sitemap.xml",
+          destination: "/sitemap"
+        },
+        {
+          source: "/robots.txt",
+          destination: "/.well-known/robots.txt"
+        },
+        {
+          source: "/keybase.txt",
+          destination: "/.well-known/keybase.txt"
+        }
+      ];
+    },
+    async redirects() {
+      return [
+        {
+          source: "/post/:path*",
+          destination: "/posts/:path*",
+          permanent: true
+        },
+        {
+          source: "/pgp",
+          destination: "https://keybase.io/maximousblk/pgp_keys.asc",
+          permanent: false
+        }
+      ];
     }
-
-    return config;
-  },
-  pwa: {
-    disable: process.env.NODE_ENV === "development",
-    dest: "public"
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/feed/:format*",
-        destination: "/api/feed?f=:format*"
-      },
-      {
-        source: "/sitemap.xml",
-        destination: "/sitemap"
-      },
-      {
-        source: "/robots.txt",
-        destination: "/.well-known/robots.txt"
-      },
-      {
-        source: "/keybase.txt",
-        destination: "/.well-known/keybase.txt"
-      }
-    ];
-  },
-  async redirects() {
-    return [
-      {
-        source: "/post/:path*",
-        destination: "/posts/:path*",
-        permanent: true
-      },
-      {
-        source: "/pgp",
-        destination: "https://keybase.io/maximousblk/pgp_keys.asc",
-        permanent: false
-      }
-    ];
-  }
-});
+  })
+);
