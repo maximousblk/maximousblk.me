3c945d20c1e6d7dcb21759cc8e968ffca0d24645
Maximous Black <maximousblk@gmail.com>
2021-10-22 21:03:05 +0000

    update code block styles

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,37 +1,41 @@
 import { useRef, useState } from "react";
-import { Copy, Check } from "react-feather";
+import { Clipboard, Check } from "react-feather";
 
 export function CodeBlock(props): JSX.Element {
   const textInput = useRef(null);
   const [hovered, setHovered] = useState(false);
   const [copied, setCopied] = useState(false);
 
-  const onEnter = () => {
+  const showCopy = () => {
     setHovered(true);
   };
 
-  const onExit = () => {
+  const hideCopy = () => {
     setHovered(false);
     setCopied(false);
   };
 
   const onCopy = () => {
-    setCopied(true);
     navigator.clipboard.writeText(textInput.current.textContent);
+    setCopied(true);
     setTimeout(() => {
       setCopied(false);
-    }, 3000);
+    }, 1000);
   };
 
   return (
-    <div ref={textInput} onMouseEnter={onEnter} onMouseLeave={onExit} className="relative">
+    <div ref={textInput} onMouseEnter={showCopy} onMouseLeave={hideCopy} className="relative">
       {hovered && (
         <button
           onClick={onCopy}
           aria-label="Copy code"
-          className="absolute right-3 top-3 items-center px-1.5 py-2 text-left rounded border border-gray-500 dark:border-coolGray-500"
+          className="absolute top-4 right-4 items-center p-1 text-left rounded border border-gray-500 dark:border-coolGray-500 hover:border-gray-700 dark:hover:border-coolGray-400"
         >
-          {copied ? <Check size={14} className="text-emerald-400" /> : <Copy size={14} className="text-gray-400" />}
+          {copied ? (
+            <Check size={12} className="text-emerald-700 dark:text-emerald-400" />
+          ) : (
+            <Clipboard size={12} className="text-gray-700 dark:text-coolGray-400" />
+          )}
         </button>
       )}
       <pre>{props.children}</pre>
