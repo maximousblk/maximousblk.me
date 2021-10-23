7ce246062bc91ab41482003e5212f40380cd2b3c
Maximous Black <maximousblk@gmail.com>
2021-10-23 09:47:47 +0000

    switch back to basic links styles

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -29,12 +29,12 @@ export function CodeBlock(props): JSX.Element {
         <button
           onClick={onCopy}
           aria-label="Copy code"
-          className="absolute top-4 right-4 items-center p-1 text-left rounded border border-gray-500 dark:border-coolGray-500 hover:border-gray-700 dark:hover:border-coolGray-400"
+          className="absolute top-4 right-4 items-center px-1 py-1.5 text-left rounded border border-gray-500 dark:border-coolGray-500 hover:border-gray-700 dark:hover:border-coolGray-400"
         >
           {copied ? (
-            <Check size={12} className="text-emerald-700 dark:text-emerald-400" />
+            <Check size={14} className="text-emerald-700 dark:text-emerald-400" />
           ) : (
-            <Clipboard size={12} className="text-gray-700 dark:text-coolGray-400" />
+            <Clipboard size={14} className="text-gray-700 dark:text-coolGray-400" />
           )}
         </button>
       )}
--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,63 +1,22 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import Icon from "@/components/FeatherIcons";
+export function CustomLink({ href, ...props }) {
+  const isInternalLink = href && (href.startsWith("/") || href.startsWith("#"));
 
-function LinkIcon({ name }) {
-  return <Icon name={name} size={16} className="inline-block ml-1 mb-1 text-gray-500 dark:text-coolGray-500" />;
-}
+  const baseStyle = "hover:underline cursor-pointer";
 
-export function CustomLink({ href, children, ...props }) {
-  const isAnchorHref = href && href.startsWith("#");
-  const isMailHref = href && href.startsWith("mailto:");
-  const isFootNoteHref = href && href.startsWith("#fn");
-  const isRelativeHref = href && (href.startsWith("/") || href.startsWith("."));
-
-  const baseStyle = "px-1 py-0.5 hover:bg-gray-100 dark:hover:bg-coolGray-800 rounded";
-
-  function BaseIconLink({ href, icon, children, ...props }) {
-    return (
-      <a href={href} className={baseStyle} {...props}>
-        {children}
-        {icon && <LinkIcon name={icon} />}
-      </a>
-    );
-  }
-
-  if (isMailHref) {
-    return (
-      <BaseIconLink href={href} icon="Mail" {...props}>
-        {children}
-      </BaseIconLink>
-    );
-  } else if (isRelativeHref) {
+  if (isInternalLink) {
     return (
       <Link href={href}>
         <a className={baseStyle} {...props}>
-          {children}
-          <LinkIcon name="Link" />
+          {props.children}
         </a>
       </Link>
     );
-  } else if (isFootNoteHref) {
-    return (
-      <a href={href} {...props}>
-        {children}
-      </a>
-    );
-  } else if (isAnchorHref) {
-    return (
-      <BaseIconLink href={href} icon="Hash" {...props}>
-        {children}
-      </BaseIconLink>
-    );
-  } else {
-    return (
-      <BaseIconLink href={href} icon={false} target="_blank" rel="noopener noreferrer" {...props}>
-        {children}
-      </BaseIconLink>
-    );
   }
+
+  return <a className={baseStyle} target="_blank" rel="noopener noreferrer" {...props} />;
 }
 
 export function Quote({ author, children, ...props }) {
