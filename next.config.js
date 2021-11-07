8391330f30f649121b3811d0ed0721afe011d073
Maximous Black <maximousblk@gmail.com>
2021-11-07 06:45:49 +0000

    use react 18 alpha

--- next.config.js
+++ next.config.js
@@ -1,53 +1,51 @@
 const withPWA = require("next-pwa");
-const withPreact = require("next-plugin-preact");
 
 /** @type {import('next').NextConfig} */
-module.exports = withPWA(
-  withPreact({
-    experimental: {
-      nftTracing: true // Fix for `ENOENT: no such file or directory, scandir '/var/task/<path>'`
-    },
-    reactStrictMode: true,
-    images: {
-      domains: ["cdn.maximousblk.me", "octodex.github.com", "twemoji.maxcdn.com"]
-    },
-    pwa: {
-      disable: process.env.NODE_ENV === "development",
-      dest: "public"
-    },
-    async rewrites() {
-      return [
-        {
-          source: "/feed/:format*",
-          destination: "/api/feed?f=:format*"
-        },
-        {
-          source: "/sitemap.xml",
-          destination: "/sitemap"
-        },
-        {
-          source: "/robots.txt",
-          destination: "/.well-known/robots.txt"
-        },
-        {
-          source: "/keybase.txt",
-          destination: "/.well-known/keybase.txt"
-        }
-      ];
-    },
-    async redirects() {
-      return [
-        {
-          source: "/post/:path*",
-          destination: "/posts/:path*",
-          permanent: true
-        },
-        {
-          source: "/pgp",
-          destination: "https://keybase.io/maximousblk/pgp_keys.asc",
-          permanent: false
-        }
-      ];
-    }
-  })
-);
+const config = {
+  nftTracing: true, // Fix for `ENOENT: no such file or directory, scandir '/var/task/<path>'`
+  swcMinify: true,
+  reactStrictMode: true,
+  images: {
+    domains: ["cdn.maximousblk.me", "octodex.github.com", "twemoji.maxcdn.com"]
+  },
+  pwa: {
+    disable: process.env.NODE_ENV === "development",
+    dest: "public"
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/feed/:format*",
+        destination: "/api/feed?f=:format*"
+      },
+      {
+        source: "/sitemap.xml",
+        destination: "/sitemap"
+      },
+      {
+        source: "/robots.txt",
+        destination: "/.well-known/robots.txt"
+      },
+      {
+        source: "/keybase.txt",
+        destination: "/.well-known/keybase.txt"
+      }
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: "/post/:path*",
+        destination: "/posts/:path*",
+        permanent: true
+      },
+      {
+        source: "/pgp",
+        destination: "https://keybase.io/maximousblk/pgp_keys.asc",
+        permanent: false
+      }
+    ];
+  }
+};
+
+module.exports = withPWA(config);
