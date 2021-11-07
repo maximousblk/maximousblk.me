360a0cecc2d17c660cad853f8c53a36c08e42fe0
Maximous Black <maximousblk@gmail.com>
2021-11-07 10:13:16 +0000

    remove /api/notion

--- pages/api/notion.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { notion, getIndex, getBlockChildren, getPage, getDatabase } from "@/lib/notion";
-
-export default async function api(req: NextApiRequest, res: NextApiResponse) {
-  const page = await getPage("df418742b19d425485e3ca47e31cfd86");
-  const blocks = await getBlockChildren(page.id);
-
-  res.json({ page, blocks });
-}
