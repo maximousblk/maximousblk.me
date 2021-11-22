7e30e8ded480add020120bee10ef3dac6a4b07ae
Maximous Black <maximousblk@gmail.com>
2021-11-22 17:45:34 +0000

    use stable react
    no use for the beta version yet

--- lib/imgsize.ts
+++ lib/imgsize.ts
@@ -1,6 +1,13 @@
 import sizeOf from "image-size";
 
-export async function getImageSize(imgUrl: string): Promise<{ width: number; height: number; orientation?: number; type?: string }> {
+interface ImageSize {
+  width: number;
+  height: number;
+  orientation?: number;
+  type?: string;
+}
+
+export async function getImageSize(imgUrl: string): Promise<ImageSize> {
   const res = await fetch(imgUrl);
   const buffer = await res.arrayBuffer();
   const dimensions = sizeOf(new Buffer(buffer));
