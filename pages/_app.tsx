48b68956940eb295d6774bfd951368082777da72
Maximous Black <maximousblk@gmail.com>
2021-11-25 17:45:06 +0000

    update umami config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,6 +26,8 @@ export default function App({ Component, pageProps }: AppProps) {
         strategy="afterInteractive"
         src="https://analytics.maximousblk.me/umami.js"
         data-website-id="d565f770-836d-42fb-9fe3-0946633cdb49"
+        data-domains="maximousblk.me"
+        data-cache="true"
       />
     </ThemeProvider>
   );
