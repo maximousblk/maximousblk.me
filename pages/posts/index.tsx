2b57ffee2ec8587fc9ebc5e8720fb399ed6f2b73
Maximous Black <maximousblk@gmail.com>
2021-12-01 14:19:53 +0000

    make borders more consistent

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -45,7 +45,7 @@ export default function Posts({ posts }) {
             type="text"
             onChange={(e) => setSearchValue(e.target.value)}
             placeholder={`Search through ${filteredPosts.length} articles`}
-            className="px-4 py-2 border border-gray-300 dark:border-gray-700 focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+            className="px-4 py-2 border border-gray-300 dark:border-gray-800 focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
           />
           <SearchIcon className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300" />
         </div>
