b717cb5a4cd6e0d8a4d9e9cc928d9cac640f410e
Maximous Black <maximousblk@gmail.com>
2021-12-03 07:32:14 +0000

    add cover image to posts

--- layouts/post.tsx
+++ layouts/post.tsx
@@ -1,6 +1,7 @@
 import { format } from "date-fns";
 import { ExternalLink } from "react-feather";
 import { NextSeo, ArticleJsonLd } from "next-seo";
+import Image from "next/image";
 
 import Container from "@/components/Container";
 import config from "@/config";
@@ -17,8 +18,11 @@ const twitterURL = (title: string, slug: string) => {
 const BlogSeo = ({ title, description, publishedAt, url, image }) => {
   const date = new Date(publishedAt).toISOString();
   const featuredImage = {
-    url: config.baseUrl + (image ?? "/og.png"),
+    url: image?.url || config.baseUrl + "/og.png",
     alt: title,
+    width: image?.width,
+    height: image?.height,
+    type: image?.type,
   };
 
   return (
@@ -53,10 +57,10 @@ const BlogSeo = ({ title, description, publishedAt, url, image }) => {
   );
 };
 
-export default function PostLayout({ children, title, slug, image, publishedAt, description, readingTime }) {
+export default function PostLayout({ children, title, slug, cover, publishedAt, description, readingTime }) {
   return (
     <Container>
-      <BlogSeo url={`${config.baseUrl}/posts/${slug}`} title={title} description={description} image={image} publishedAt={publishedAt} />
+      <BlogSeo url={`${config.baseUrl}/posts/${slug}`} title={title} description={description} image={cover} publishedAt={publishedAt} />
       <article className="flex flex-col justify-center items-start max-w-4xl mx-auto mb-16 w-full">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">{title}</h1>
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-2 mb-8">
@@ -65,6 +69,11 @@ export default function PostLayout({ children, title, slug, image, publishedAt, 
           </div>
           <p className="text-sm font-mono text-gray-600 dark:text-gray-400 min-w-32 mt-2 md:mt-0">{readingTime}</p>
         </div>
+        {cover && (
+          <div className="flex mb-8 w-full h-72 rounded overflow-hidden">
+            <Image src={cover.url} alt="" width={cover.width} height={cover.height} className="object-cover" />
+          </div>
+        )}
         <div className="prose dark:prose-dark max-w-none w-full">{children}</div>
         <div className="flex space-x-3 text-sm text-gray-700 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-200 mt-8">
           <a href={twitterURL(title, slug)} target="_blank" rel="noopener noreferrer" className="hover:underline">
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -3,14 +3,15 @@ import { getBlockChildren, getDatabase, getIndex, getReadingTime } from "@/lib/n
 import { NotionContent } from "@/lib/render";
 import { parseISO } from "date-fns";
 import type { GetStaticPropsContext, GetStaticPropsResult } from "next";
+import getImageSize from "probe-image-size/sync";
 
-export default function Blog({ blocks, slug, title, description, image, publishedAt }) {
+export default function Blog({ blocks, slug, title, description, cover, publishedAt }) {
   return (
     <PostLayout
       title={title}
       slug={slug}
       description={description}
-      image={image}
+      cover={cover}
       publishedAt={publishedAt}
       readingTime={getReadingTime(blocks).text}
     >
@@ -26,14 +27,20 @@ export async function getStaticProps({ params: { slug } }: GetStaticPropsContext
   const post = posts.results.find(({ properties: { slug: sl } }) => sl[sl.type].map(({ plain_text }) => plain_text).join("__") === slug);
   if (!post) return { notFound: true };
 
-  const { title: postTitle, description: postDescription, date } = post.properties;
+  const { title: postTitle, description: postDescription, date, cover: coverImage } = post.properties;
 
   const blocks = await getBlockChildren(post.id);
   const title = postTitle[postTitle.type].map(({ plain_text }) => plain_text).join(" ");
   const description = postDescription[postDescription.type].map(({ plain_text }) => plain_text).join(" ");
   const publishedAt = parseISO(date[date.type].start).getTime();
+  const coverURL = coverImage[coverImage.type][0]?.file.url;
+  const cover = coverURL
+    ? await fetch(coverURL)
+        .then((res) => res.arrayBuffer())
+        .then((data) => ({ url: coverURL, ...getImageSize(Buffer.from(data)) }))
+    : null;
 
-  return { props: { blocks, title, description, slug, publishedAt }, revalidate: 2700 };
+  return { props: { blocks, slug, title, description, cover, publishedAt }, revalidate: 2700 };
 }
 
 export async function getStaticPaths() {
