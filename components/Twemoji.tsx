4b921db08c4a1f052e298e8e3c0f7d2bda9e0a0b
Maximous Black <maximousblk@gmail.com>
2022-05-14 08:10:37 +0000

    fix: increase nav logo size

--- components/Twemoji.tsx
+++ components/Twemoji.tsx
@@ -5,8 +5,8 @@ export function getEmojiLink(emoji: string) {
   return `https://twemoji.maxcdn.com/v/latest/svg/${emoji.codePointAt(0).toString(16)}.svg`;
 }
 
-export function Twemoji({ emoji, size = 16, ...props }: { emoji: string; size?: number } & Omit<ImageProps, "src" | "height" | "width">) {
-  return <Image src={getEmojiLink(emoji)} height={size} width={size} aria-hidden="true" alt={emoji} {...props} />;
+export function Twemoji({ emoji, size = 18, ...props }: { emoji: string; size?: number } & Omit<ImageProps, "src" | "height" | "width">) {
+  return <Image src={getEmojiLink(emoji)} height={size} width={size} aria-hidden="true" alt="" {...props} />;
 }
 
 export default memo(Twemoji);
