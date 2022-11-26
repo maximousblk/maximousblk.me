1480bc318f3e6cb1aab4ce970ddfb8e119f36386
Maximous Black <maximousblk@gmail.com>
2022-11-26 21:57:13 +0530

    narrow navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,12 +10,12 @@ export default function Navbar() {
   return (
     <NavWrapper>
       <div className="flex w-full max-w-6xl flex-nowrap items-center justify-between">
-        <div className="flex flex-shrink-0">
+        <div className="flex flex-shrink-0 space-x-0 md:space-x-2">
           {config.nav.map(({ name, emoji, href }, i, a) => (
-            <div key={name} className="my-3 inline-block">
+            <div key={name} className="my-1 inline-block">
               <Link
                 href={href}
-                className="flex items-center justify-between rounded px-2.5 py-1.5 text-gray-900 hover:bg-gray-100 dark:text-gray-100 dark:hover:bg-gray-800"
+                className="flex items-center justify-between rounded px-2.5 py-1.5 text-gray-900 hover:bg-gray-100/20 dark:text-gray-100 dark:hover:bg-gray-800/20"
               >
                 <span className="flex select-none items-center justify-between pr-2 text-sm" aria-hidden="true">
                   <Twemoji priority emoji={nameToEmoji[emoji]} />
