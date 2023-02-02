19b27c1d37c9247f1c8bc1acde46028bc9128415
Maximous Black <maximousblk@gmail.com>
2023-02-02 22:35:53 +0530

    mono date font

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -15,7 +15,7 @@ export default function BlogPost({ title, description, slug, publishedAt }: Attr
         <div className="mb-4 mt-8 w-full">
           <div className="flex flex-col justify-between md:flex-row">
             <h4 className="mb-2 w-full text-lg font-medium text-gray-900 dark:text-gray-100 md:text-xl">{title}</h4>
-            <p className="mb-4 w-32 text-left text-gray-500 dark:text-gray-500 md:mb-0 md:text-right">
+            <p className="mb-4 w-32 text-left font-mono text-gray-500 dark:text-gray-500 md:mb-0 md:text-right">
               {publishedAt && format(publishedAt, "MMM, yyy")}
             </p>
           </div>
