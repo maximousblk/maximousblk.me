691bc6ebd895c1d8214c07a57c569fd29f37b2cc
Maximous Black <maximousblk@gmail.com>
2023-02-06 06:55:00 +0530

    feat: add loading state to theme switch

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
-import { nameToEmoji } from "gemoji";
 
 import Icon from "@/components/FeatherIcons";
 
@@ -14,17 +13,18 @@ export default function ThemeSwitch() {
   useEffect(() => setMounted(true), []);
 
   return (
-    mounted && (
-      <div className="flex flex-shrink-0 space-x-2">
-        <button
-          aria-label="Toggle Dark Mode"
-          type="button"
-          className="h-10 w-10 rounded border border-transparent p-3 text-gray-900 hover:border-gray-500/10 hover:bg-gray-200/30 dark:text-gray-100 dark:hover:bg-gray-700/30"
-          onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
-        >
+    <div className="flex flex-shrink-0 space-x-2">
+      <button
+        aria-label="Toggle Dark Mode"
+        disabled={!mounted}
+        type="button"
+        className="h-10 w-10 rounded border border-transparent p-3 text-gray-900 hover:border-gray-500/10 hover:bg-gray-200/30 dark:text-gray-100 dark:hover:bg-gray-700/30"
+        onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+      >
+        {(mounted && (
           <Icon name={resolvedTheme === "dark" ? "FiSun" : "FiMoon"} className="h-4 w-4 text-gray-800 dark:text-gray-200" />
-        </button>
-      </div>
-    )
+        )) || <Icon name="FiLoader" className="h-4 w-4 animate-spin text-gray-800 dark:text-gray-200" />}
+      </button>
+    </div>
   );
 }
